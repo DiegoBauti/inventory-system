81459ecb5607034843fcab7e18973cf67ec87176
Diego Bautista <bautista.diegomlp@gmail.com>
2025-11-22 00:23:45 -0500

    feat(repository): add CRUD repositories for domain entities

---
+++ src/main/java/Repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package Repository;
+
+import Entity.Product;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends CrudRepository<Product,Integer> {
+}
---
+++ src/main/java/Repository/SupplierRepository.java
@@ -0,0 +1,9 @@
+package Repository;
+
+import Entity.Supplier;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SupplierRepository extends CrudRepository<Supplier,Integer> {
+}
